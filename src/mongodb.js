09dc455608787e56e791edc99b388a47cb6abf01
EliavC <eliavc2001@gmail.com>
2023-06-22 14:36:55 +0300

    Revert "updated server to insert and amount"
    This reverts commit 123e48279f2d8fe666b6127a4d0c2459c937fcb3.

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,4 +1,3 @@
-//const { Collection } = require("mongodb");
 const mongoose=require("mongoose")
 
 //mongoose.connect("mongodb://0.0.0.0:27017/OurShop")
@@ -10,7 +9,7 @@ mongoose.connect("mongodb+srv://eliav:2001@ourshop.vtknxmb.mongodb.net/?retryWri
 .catch(()=>{
     console.log('failed');
 })
-
+ 
 const LogInSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -22,34 +21,7 @@ const LogInSchema=new mongoose.Schema({
     }
 })
 
-const chair = new mongoose.Schema({
-    nameChair:{
-        type:String,
-        required:true
-    },
-    color:{
-        type:String,
-        required:true
-    },
-    matter:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    amount:{
-        type:Number,
-        required:true
-    },
-    pic:{
-        type:String,
-        required:true
-    }
-})
-
-const bed = new mongoose.Schema({
+const object = new mongoose.Schema({
     name:{
         type:String,
         required:true
@@ -74,193 +46,17 @@ const bed = new mongoose.Schema({
         type:String,
         required:true
     },
-    size:{
+    category:{
         type:String,
+        //enum:['chair','bed','couch','mirror','rug','table'],
         required:true
     }
 })
 
-const table = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    color:{
-        type:String,
-        required:true
-    },
-    matter:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    amount:{
-        type:Number,
-        required:true
-    },
-    pic:{
-        type:String,
-        required:true
-    },
-    size:{
-        type:String,
-        required:true
-    }
-})
 
-const couch = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    color:{
-        type:String,
-        required:true
-    },
-    matter:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    amount:{
-        type:Number,
-        required:true
-    },
-    pic:{
-        type:String,
-        required:true
-    },
-    size:{
-        type:String,
-        required:true
-    }
-})
-
-const mirror = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    color:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    amount:{
-        type:Number,
-        required:true
-    },
-    pic:{
-        type:String,
-        required:true
-    },
-    size:{
-        type:String,
-        required:true
-    }
-})
-
-const rug = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    size:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    color:{
-        type:String,
-        required:true
-    },
-    amount:{
-        type:Number,
-        required:true
-    },
-    pic:{
-        type:String,
-        required:true
-    }
-})
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*const rugCollect = new mongoose.model("rrugs",rug)
-module.exports = rugCollect
-
-const mirrorCollect = new mongoose.model("mmirrors",mirror)
-module.exports=mirrorCollect
-
-const couchCollect = new mongoose.model("ccouchs",couch)
-module.exports=couchCollect
-
-const tableCollect = new mongoose.model("ttables",table)
-module.exports=tableCollect
-
-const bedCollect = new mongoose.model("bbeds",bed)
-module.exports=bedCollect
-
-const chairCollect = new mongoose.model("cchairs",chair)
-=======
-=======
->>>>>>> parent of 123e482 (updated server to insert and amount)
-/*const rugCollect = new mongoose.model("rug",rug)
-module.exports = rugCollect
-
-const mirrorCollect = new mongoose.model("mirror",mirror)
-module.exports=mirrorCollect
-
-const couchCollect = new mongoose.model("couch",couch)
-module.exports=couchCollect
-
-const tableCollect = new mongoose.model("table",table)
-module.exports=tableCollect
-
-const bedCollect = new mongoose.model("bed",bed)
-module.exports=bedCollect
-
-
-const chairCollect = new mongoose.model("chair",chair)
-<<<<<<< HEAD
->>>>>>> parent of 123e482 (updated server to insert and amount)
-=======
->>>>>>> parent of 123e482 (updated server to insert and amount)
-module.exports=chairCollect
-
-const collection=new mongoose.model("users",LogInSchema)
-module.exports=collection*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-const chairCollect = new mongoose.model("chairs",chair)
+const objectCollection = new mongoose.model("objects",object)
 const collection=new mongoose.model("users",LogInSchema)
 module.exports = {
-    collection,chairCollect
+    collection,objectCollection
 }
 
-
-=======
-=======
->>>>>>> parent of 123e482 (updated server to insert and amount)
-const collection=new mongoose.model("users",LogInSchema)
-const chairCollect = new mongoose.model("chairs",chair)
-module.exports={
-    collection,chairCollect
-<<<<<<< HEAD
-}
->>>>>>> parent of 123e482 (updated server to insert and amount)
-=======
-}
->>>>>>> parent of 123e482 (updated server to insert and amount)
